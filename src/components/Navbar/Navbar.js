7d6f356a8aa04cd8d7502d6c104949337f2b9e47
hiiam-andy <andrew.citb@gmail.com>
2023-08-06 11:44:48 +0300

    фикс

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,5 @@
 import React, { useContext } from "react";
-import { useNavigate } from "react-router-dom";
 import { Context } from "../../index";
-import { LOGIN_ROUTE } from "../../utils/consts";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 import MyButton from "../UI/MyButton/MyButton";
@@ -9,22 +7,12 @@ import MyButton from "../UI/MyButton/MyButton";
 import style from "./Navbar.module.css";
 
 function Navbar() {
-  const navigate = useNavigate();
   const { auth } = useContext(Context);
   const [user] = useAuthState(auth);
   return (
     <div class={style.navbar_container}>
       <h2 className={style.logo}>SuperChat</h2>
-
-      {user ? (
-        <MyButton onClick={() => auth.signOut()} variant="contained">
-          Выйти
-        </MyButton>
-      ) : (
-        <MyButton variant="contained" onClick={() => navigate(LOGIN_ROUTE)}>
-          Логин
-        </MyButton>
-      )}
+      {user && <MyButton onClick={() => auth.signOut()}>Выйти</MyButton>}
     </div>
   );
 }
